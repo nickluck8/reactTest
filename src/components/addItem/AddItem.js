3b0d65c562e06bf15107752f557a677b32478c2c
Mykola Steliuk <msteliuk@rubiconproject.com>
2019-04-05 16:46:45 +0300

    first commit

---
+++ src/components/addItem/AddItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class AddItem extends React.Component {
+    render() {
+        return (
+            <div className="item-add-form">
+                <button className="btn btn-success">Add Item</button>
+            </div>
+        )
+    }
+}
